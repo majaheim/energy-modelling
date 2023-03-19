4dd1e339c65855268c734eacad83da6fb800d9ad
Maja <e11810336@tuwien.ac.at>
2023-03-19 15:14:41 +0100

    unit test updated 'open mat file'

--- test_source.py
+++ test_source.py
@@ -2,4 +2,4 @@ import source
 
 def test_open_mat_data():
     ESPV = source.open_mat_data('data/PV_Einspeiseprofil.mat', 'Leistung_Vec_Temperatur_Temp')
-    assert ESPV[33] == 0.0151596888838696
+    assert round(ESPV[33], 5) == round(0.0151596888838696, 5)
