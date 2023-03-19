b10087060b6deaeba478a400b63835fb6f4faf8e
Maja <e11810336@tuwien.ac.at>
2023-03-19 15:39:40 +0100

    updated unit test

--- test_source.py
+++ test_source.py
@@ -2,8 +2,8 @@ import source
 
 def test_open_mat_data():
     ESPV = source.open_mat_data('data/PV_Einspeiseprofil.mat', 'Leistung_Vec_Temperatur_Temp')
-    assert graph_single_array(35040, ESPV , "Einspeiseprofil der PV Anlage")
+    assert round(ESPV[33], 5) == round(0.0151596888838696, 5)
     
 def test_graph_single_array():
     ESPV = source.open_mat_data('data/PV_Einspeiseprofil.mat', 'Leistung_Vec_Temperatur_Temp')
-    assert graph_single_array(35040, ESPV , "Einspeiseprofil der PV Anlage") == True
+    assert source.graph_single_array(35040, ESPV , "Einspeiseprofil der PV Anlage") == True
