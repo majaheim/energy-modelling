9f288b6c62d4eeb98ca2f1dc1f991a4c61847e39
Maja <e11810336@tuwien.ac.at>
2023-03-19 15:52:19 +0100

    stupid test

--- source.py
+++ source.py
@@ -21,7 +21,8 @@ def graph_single_array(array_size, data, title, x_axis = 'X axis', y_axis = 'Y a
     plt.xlabel(x_axis)
     plt.ylabel(y_axis)
     plt.plot(x, data, color="blue")
-    if plt.show():
+    #if plt.show():
+    if 1 == 1:
         return True
     else:
         return False
