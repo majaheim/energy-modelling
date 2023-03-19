ea5b2fa78ad4e1082df2b1c2243b62dce997dfdd
Maja <e11810336@tuwien.ac.at>
2023-03-19 13:55:33 +0100

    added Einspeise Plot

--- source.py
+++ source.py
@@ -1,3 +1,23 @@
 import scipy.io
-mat = scipy.io.loadmat('PV_Einspeiseprofil.mat')
+import numpy as np
+import matplotlib.pyplot as plt
+mat = scipy.io.loadmat('PV_Einspeiseprofil.mat', squeeze_me = True)
 print(mat)
+np.transpose(mat)
+#print(mat['Leistung_Vec_Temperatur_Temp'])
+E = mat['Leistung_Vec_Temperatur_Temp']
+#E = E.tolist()
+print(E)
+
+# test plot
+x = np.arange(0, 35040)
+# x = np.linspace(0, 35039, 35040)
+# np.transpose(x)
+print(x)
+
+
+plt.title("Line graph")
+plt.xlabel("X axis")
+plt.ylabel("Y axis")
+plt.plot(x, E, color ="red")
+plt.show()
