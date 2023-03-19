68306d9f09cf84d62899a9b2080311f6ddbef31a
Maja <e11810336@tuwien.ac.at>
2023-03-19 14:17:29 +0100

    function that opens mat file added

--- source.py
+++ source.py
@@ -1,23 +1,22 @@
 import scipy.io
 import numpy as np
 import matplotlib.pyplot as plt
-mat = scipy.io.loadmat('PV_Einspeiseprofil.mat', squeeze_me = True)
-print(mat)
-np.transpose(mat)
-#print(mat['Leistung_Vec_Temperatur_Temp'])
-E = mat['Leistung_Vec_Temperatur_Temp']
-#E = E.tolist()
-print(E)
 
-# test plot
-x = np.arange(0, 35040)
-# x = np.linspace(0, 35039, 35040)
-# np.transpose(x)
-print(x)
+def open_mat_data(file_name, field_name):
+    mat = scipy.io.loadmat(file_name, squeeze_me = True)
+    return mat[field_name]
 
+def main():
+    print("Hello World!")
+    ESPV = open_mat_data('PV_Einspeiseprofil.mat', 'Leistung_Vec_Temperatur_Temp')
 
-plt.title("Line graph")
-plt.xlabel("X axis")
-plt.ylabel("Y axis")
-plt.plot(x, E, color ="red")
-plt.show()
+    # test plot
+    x = np.arange(0, 35040)
+    plt.title("Einspeiseprofil der PV Anlage")
+    plt.xlabel("X axis")
+    plt.ylabel("Y axis")
+    plt.plot(x, ESPV, color ="red")
+    plt.show()
+
+if __name__ == "__main__":
+         main()
