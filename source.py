f24d420d85fec15d87deca3acc21c29eb27daea4
Maja <e11810336@tuwien.ac.at>
2023-04-06 19:41:06 +0200

    general cleanup

--- source.py
+++ source.py
@@ -34,6 +34,16 @@ def graph_single_array(array_size, data, title, x_axis = 'X axis', y_axis = 'Y a
 # I am aware that this return makes little sense - I just had to create this in order to have a sucessfull unit test
 # The unit test makes sure that a plot was created, but cannot make assumptions about the correctness of the data in the plot
 
+def graph_multiple_array(array_size, data, data2, title = 'Example', x_axis = 'X axis', y_axis = 'Y axis', description = 'data', description2 = 'data2'):
+    x = np.arange(0, array_size)
+    plt.title(title)
+    plt.xlabel(x_axis)
+    plt.ylabel(y_axis)
+    plt.plot(x, data, color="blue", label = description)
+    plt.plot(x, data2, color="red", label = description2)
+    plt.legend()
+    plt.show()
+    return True
 
 #Function that creates an array of zeros
 def zeros(size):
@@ -42,30 +52,30 @@ def zeros(size):
     return array
 
 #Function that checks what PV is producing and fills the arrays
-def check_pv_production(size, pv_power, c_power, c_houshold):
-    excess = zeros(size)
-    #pv_power = zeros(size)
-    #c_power = zeros(size)
-    #c_houshold = zeros(size)
-
-    for n in range(2, size, 1):
-        if pv_power[n] >= c_power[n]:   #pv produces more than what is needed by the household
-            c_houshold[n] = c_power[n]
-            excess[n] = pv_power[n] - c_power[n]
-        elif n == 999:
-            print('hi')
-        else:
-            c_houshold[n] = c_power[n]
-            excess[n] = 0
-        household = np.array([c_houshold, excess])
-
-    return household
+# def check_pv_production(size, pv_power, c_power, c_houshold):
+#     excess = zeros(size)
+#     #pv_power = zeros(size)
+#     #c_power = zeros(size)
+#     #c_houshold = zeros(size)
+#
+#     for n in range(2, size, 1):
+#         if pv_power[n] >= c_power[n]:   #pv produces more than what is needed by the household
+#             c_houshold[n] = c_power[n]
+#             excess[n] = pv_power[n] - c_power[n]
+#         elif n == 999:
+#             print('hi')
+#         else:
+#             c_houshold[n] = c_power[n]
+#             excess[n] = 0
+#         household = np.array([c_houshold, excess])
+#
+#     return household
 
 #Function that fills workday the array with 1(workday) or 0(weekend)
-def init(workday_array):
+def init(workday_array, days):
     counter = 1
-    for n in range(0,365,1):
-        if counter <= 5:        #Monday - Friday
+    for n in range(0, 365, 1):
+        if counter <= days:        #Monday - Friday
             workday_array[n] = 1
         else:
             workday_array[n] = 0
@@ -73,34 +83,34 @@ def init(workday_array):
         if counter == 7:
             counter = 1
         else:
-            counter = counter +1
+            counter = counter + 1
 
     return workday_array
 
 #Function that configures car availability array
-def car_availability(car_array, workday_array):
-    for n in range(0,365,1):
-        if workday_array[n] == 1:   #Mo-Fr
-            if (96*(n-1)) == 0:          #24h*4(power is measeured 4 times per hour)
-                for i in range(96):
-                    if i <= 34 | i >= 70:    #car is at home (only first day)
-                        car_array[i] = 1
-            else:
-                for i in range(96*(n-1), 96*n, 1):
-                    if (i <= (34 + 96*(n-1))) | (i >= (70 + 96*(n-1))):
-                        car_array[i] = 1
-        else:                       #Weekend
-            for i in range(96*(n-1), 96*n, 1):
-                car_array[i] = 1            #car is at home all weekend
-
-    return car_array
+# def car_availability(car_array, workday_array):
+#     for n in range(0,365,1):
+#         if workday_array[n] == 1:   #Mo-Fr
+#             if (96*(n-1)) == 0:          #24h*4(power is measeured 4 times per hour)
+#                 for i in range(96):
+#                     if i <= 34 | i >= 70:    #car is at home (only first day)
+#                         car_array[i] = 1
+#             else:
+#                 for i in range(96*(n-1), 96*n, 1):
+#                     if (i <= (34 + 96*(n-1))) | (i >= (70 + 96*(n-1))):
+#                         car_array[i] = 1
+#         else:                       #Weekend
+#             for i in range(96*(n-1), 96*n, 1):
+#                 car_array[i] = 1            #car is at home all weekend
+#
+#     return car_array
 
 def main():
     print("Hello World!")
 
-    #load PV Einspeiseprofildata
-    ESPV = open_mat_data('data/PV_Einspeiseprofil.mat', 'Leistung_Vec_Temperatur_Temp')
-    #graph_single_array(35040, ESPV, "Einspeiseprofil der PV Anlage")
+    # #load PV Einspeiseprofildata
+    # ESPV = open_mat_data('data/PV_Einspeiseprofil.mat', 'Leistung_Vec_Temperatur_Temp')
+    # graph_single_array(35040, ESPV, "Einspeiseprofil der PV Anlage")
 
     # load LeistungHaushalte - Consumer 8 is used here
     LH8 = open_mat_data('data/LeistungHaushalte.mat', 'LeistungHaushalte', 8)
@@ -112,27 +122,37 @@ def main():
     # load Spotpreis
     #LH = open_mat_data('data/LeistungHaushalte.mat', 'LeistungHaushalte')
 
-    #The section below intializes variables used for further calculations
-    PV_power = 10*ESPV*1000*0.25     #1000: Power is kW , 0.25:??
-    Battery_state = zeros(35040)
-    Charging_energy = zeros(35040)
-    Dischargeing_energy = zeros(35040)
-    Own_consumption_H8 = zeros(35040)
+    # #The section below intializes variables used for further calculations
+    # PV_power = 10*ESPV*1000*0.25     #1000: Power is kW , 0.25:??
+    # Battery_state = zeros(35040)
+    # Charging_energy = zeros(35040)
+    # Dischargeing_energy = zeros(35040)
+    # Own_consumption_H8 = zeros(35040)
 
-    #At the begining the Battery is full:
-    Battery_state[0] = 58000
+    # #At the begining the Battery is full:
+    # Battery_state[0] = 58000
 
-    Workday = zeros(365)
-    Workday = init(Workday)
-    Car_available = zeros(35040)
-    Car_available = car_availability(Car_available, Workday)
+    Workday_5 = zeros(365)
+    Workday_5 = init(Workday_5, 5)
+    Workday_5_short = Workday_5[0:13]
+    #graph_single_array(13, Workday_5_short, "Availability EV 5 days a week")
 
-    #Laden ??
+
+    Workday_4 = zeros(365)
+    Workday_4 = init(Workday_4, 4)
+    Workday_4_short = Workday_4[0:13]
+
+    graph_multiple_array(13, Workday_5_short, Workday_4_short, "EV Availability - 2 week period", "Days", "Availability", "5 workdays", "4 workdays")
+
+    # Car_available = zeros(35040)
+    # Car_available = car_availability(Car_available, Workday_5)
+    # Car_available_short = Car_available[0:672]      #one week
+    # graph_single_array(672, Car_available_short, "Car availability")
 
     #Calculations for 9-5Job Unidirectioinal
-    H8 = check_pv_production(35040, PV_power, LH8, Own_consumption_H8)  #Index 0: houshold consumption, Index 1: excess power
-    #print(H8.shape)
-    graph_single_array(35040, Car_available, 'test')
+    # H8 = check_pv_production(35040, PV_power, LH8, Own_consumption_H8)  #Index 0: houshold consumption, Index 1: excess power
+    # #print(H8.shape)
+    # graph_single_array(35040, Car_available, 'test')
 
 
 
--- test_source.py
+++ test_source.py
@@ -8,12 +8,23 @@ def test_graph_single_array():
     ESPV = source.open_mat_data('data/PV_Einspeiseprofil.mat', 'Leistung_Vec_Temperatur_Temp')
     assert source.graph_single_array(35040, ESPV, "Einspeiseprofil der PV Anlage") == True
 
+def test_graph_multiple_array():
+    ESPV = source.open_mat_data('data/PV_Einspeiseprofil.mat', 'Leistung_Vec_Temperatur_Temp')
+    LH8 = source.open_mat_data('data/LeistungHaushalte.mat', 'LeistungHaushalte', 8)
+    assert source.graph_multiple_array(35040, ESPV, LH8, "Einspeiseprofil der PV Anlage") == True
+
 def test_zeros():
     array = source.zeros(378)
     assert array[45] == 0
 
 def test_init():
     array = source.zeros(365)
-    array = source.init(array)
+    array = source.init(array, 5)
     print(array[3])
     assert array[4] == 1
+
+# def test_car_availability():
+#     car = source.zeros(365)
+#     workday = source.zeros(365)
+#     car = source.car_availability(car, workday)
+#     assert car[30] == 1
