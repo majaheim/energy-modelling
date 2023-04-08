b32a470e9bf988d841a90157b125e3f48a07ddbb
Maja <e11810336@tuwien.ac.at>
2023-04-08 18:43:55 +0200

    stylized dataset for test added

--- test_source.py
+++ test_source.py
@@ -3,8 +3,8 @@ import source
 
 """test open_mat_data"""
 def test_open_mat_data():
-    ESPV = source.open_mat_data('data/PV_Einspeiseprofil.mat', 'Leistung_Vec_Temperatur_Temp')
-    assert round(ESPV[33], 5) == round(0.0151596888838696, 5)
+    power_household = source.open_mat_data('data/LeistungHaushalte_fortest.mat', 'LeistungHaushalte')
+    assert round(power_household[3], 5) == round(1.057154164971246, 5)
 
 def test_graph_single_array():
     ESPV = source.open_mat_data('data/PV_Einspeiseprofil.mat', 'Leistung_Vec_Temperatur_Temp')
@@ -12,7 +12,7 @@ def test_graph_single_array():
 
 def test_graph_multiple_array():
     ESPV = source.open_mat_data('data/PV_Einspeiseprofil.mat', 'Leistung_Vec_Temperatur_Temp')
-    LH8 = source.open_mat_data('data/LeistungHaushalte.mat', 'LeistungHaushalte', 8)
+    LH8 = source.open_mat_data('data/LeistungHaushalte_fortest.mat', 'LeistungHaushalte', 8)
     assert source.graph_multiple_array(35040, ESPV, LH8, "Einspeiseprofil der PV Anlage") == True
 
 def test_zeros():
